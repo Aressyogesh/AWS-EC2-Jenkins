813e4bd9e7e4bbc9df31d0e06b763242462c5677
Yogesh <yogesh.lolage@aress.com>
2022-05-18 15:43:55 +0530

    chrome path updated

--- src/main/java/Core/StepBase.java
+++ src/main/java/Core/StepBase.java
@@ -70,7 +70,8 @@ public class StepBase {
 	{	  
 		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")  + "/Resources/chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")  + "/usr/bin/chromedriver");
-		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/EQ2-HEMSWEB/Resources/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/EQ2-HEMSWEB/Resources/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
 		options.addArguments("disable-gpu");
